e93aefdf0c9eae451843e3a19d1f7ffb0860f965
Frank Flores Rutti <ffloresrutti@gmail.com>
2021-05-08 16:01:04 -0500

    Unpdate Script Jump

--- Unity+GitHub-TEST/Assets/Frank/JumpScript.cs
+++ Unity+GitHub-TEST/Assets/Frank/JumpScript.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class JumpScript : MonoBehaviour
 {
-    //This comment
-    //Second comment
-    //Third comment
-    // Start is called before the first frame update
+    Transform _transform;
+
     void Start()
     {
-        
+        _transform = GetComponent<Transform>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _transform.position += new Vector3(0f, 0f, 0.02f);
     }
 }
